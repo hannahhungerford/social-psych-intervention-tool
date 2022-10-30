e690731c63b78616f8673dc27c9bf69c497819ce
hannahwattpad <hannah.hungerford@wattpad.com>
2022-10-30 13:05:04 -0400

    set up base website

---
+++ components/resources.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '@styles/content.module.css'
+import StarIcon from '@mui/icons-material/Star';
+import Image from 'next/image';
+
+function Resources() {
+  return (
+    <div className={styles.recipeBox}>
+    </div>
+  )
+}
+
+export default Resources;
---
+++ components/test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '@styles/content.module.css'
+import StarIcon from '@mui/icons-material/Star';
+import Image from 'next/image';
+
+function Test() {
+  return (
+    <div className={styles.recipeBox}></div>
+  )
+}
+
+export default Test;
